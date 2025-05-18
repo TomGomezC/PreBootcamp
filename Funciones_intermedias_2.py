b4b72b6e445e6dd69df503b7b9aff8046bbb4fe8
tomas <tomasfelipe.gomezc@gmail.com>
2025-05-18 18:26:53 -0400

    Tarea Funciones intermedias  - Iterar a través de una lista de diccionarios

---
+++ Funciones_intermedias_2.py
@@ -0,0 +1,9 @@
+cantantes = [
+    {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
+    {"nombre": "Chayanne", "pais": "Puerto Rico"},
+    {"nombre": "José José", "pais": "México"},
+    {"nombre": "Juan Luis Guerra", "pais": "República Dominicana"}
+]
+
+for n in cantantes:
+    print("nombre - " ,n["nombre"], "," , "pais - ",n["pais"])
